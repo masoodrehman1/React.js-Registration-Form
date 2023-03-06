b57c327931ccd81b220425bce07a48c2a7118732
Masood Ur Rehman <maasoodrehman@gmail.com>
2023-03-07 00:03:32 +0500

    I have made registeration form on react using reactstrap

---
+++ src/components/SocialButton.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const SocialButton = () => {
+  return (
+    <div>
+    <button
+    id="connect-facebook"
+    onClick=" connectWithSocialMedia(event)"
+  >
+    Connect with Facebook
+  </button>
+  <button
+    id="connect-twitter"
+    onClick=" connectWithSocialMedia(event)"
+  >
+    Connect with Twitter
+  </button>
+  <button
+    id="connect-instagram"
+    onClick=" connectWithSocialMedia(event)"
+  >
+    Connect with Instagram
+  </button>
+    </div>
+  )
+}
+
+export default SocialButton
