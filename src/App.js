8844ae26c9f4c7afb4a4311d7e8890a789caba06
Masood Ur Rehman <rehmamas@corp.contoursoftware.net>
2023-03-08 14:15:38 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import  ReactDOM  from 'react-dom';
+// import './App.css';
+
+// import Body from './components/body'
+
+import Inputs from './components/inputs';
+
+
+function App() {
+
+
+
+  return (
+    <div>
+     <Inputs/>
+     
+     </div>
+  );
+}
+
+export default App;
