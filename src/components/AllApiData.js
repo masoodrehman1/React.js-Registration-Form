473c608feedb972b22a8e7d3d2819b53ce4322b7
Masood Ur Rehman <maasoodrehman@gmail.com>
2023-04-03 14:43:40 +0500

    here i have used express js and added delete and edit operations

--- src/components/AllApiData.js
+++ src/components/AllApiData.js
@@ -1,21 +1,50 @@
-import {useEffect} from 'react'
-import {  useDispatch, useSelector } from 'react-redux';
+import { useEffect, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { FetchApi } from './FetchApi';
-import { fetchApiUsers } from '../ReduxData/Reducers/Reducer1';
+import { fetchApiUsers, deleteData, clearApiUser } from '../ReduxData/Reducers/Reducer1';
 
 const AllApiData = () => {
-    const dispatch= useDispatch()
-    const usersInputs= useSelector(state=>state.users.formData)
-    useEffect(()=>{
-     FetchApi.postUsers(usersInputs)
-    },[usersInputs])
-    useEffect(() => {
-        FetchApi.getUsers().then((data)=>dispatch(fetchApiUsers(data))) 
-            }, [])
+  const dispatch = useDispatch();
+  const { formData, apiUser } = useSelector(state => state.users);
 
+  const getUsers = useCallback(() => {
+    FetchApi.getUsers().then((data) => dispatch(fetchApiUsers(data)));
+  }, [dispatch]);
 
-  return null
-}
+  
+  const deleteUser = useCallback((id) => {
+    FetchApi.deleteuser(id).then(() => dispatch(deleteData(id)));
+  }, [dispatch]);
 
-export default AllApiData
+  const putUser = useCallback(() => {
+    formData.forEach((record) => {
+      FetchApi.putUser(record.id, record);
+    });
+  }, [formData]);
 
+  useEffect(() => {
+    getUsers();
+  }, [getUsers]);
+
+  useEffect(() => {
+    if (apiUser.length > 0) 
+      return FetchApi.postUsers(apiUser)
+        .then((data) => dispatch(fetchApiUsers(data)));
+    }, [apiUser, dispatch])
+     
+    
+ 
+
+  useEffect(() => {
+    return () => {
+      putUser();
+    };
+  }, [putUser]);
+  const handleDelete = (id) => {
+    deleteUser(id);
+  };
+
+  return null;
+};
+
+export default AllApiData;
