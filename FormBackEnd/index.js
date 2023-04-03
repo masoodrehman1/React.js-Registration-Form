8436e0ca8981f0c6048b7c0bdcb3e6b18cd49f41
Masood Ur Rehman <maasoodrehman@gmail.com>
2023-04-03 14:42:43 +0500

    i have used express js and also added edit operation to api and delete operation

--- FormBackEnd/index.js
+++ FormBackEnd/index.js
@@ -1,76 +1,137 @@
 const http = require("http");
 const fs = require("fs");
 const cors = require("cors");
+const express=require("express")
+const app = express()
 
-const server = http.createServer((req, res) => {
-  cors()(req, res, () => {
-    if (req.method === "GET" && req.url === "/usersApi") {
-      fs.readFile(`${__dirname}/usersData.json`, "utf-8", (err, data) => {
-        if (err) {
-          res.writeHead(404, { "Content-Type": "text/html" });
-          res.end("error occurs");
-        } else {
-          res.writeHead(200, { "Content-Type": "application/json" });
-          res.end(data);
-        }
-      });
-    } else if (req.method === "GET" && req.url.startsWith("/usersApi")) {
-      const id = parseInt(req.url.split("/")[2]);
-      fs.readFile(`${__dirname}/usersData.json`, "utf-8", (err, data) => {
-        if (err) {
-          res.writeHead(404, { "Content-Type": "text/html" });
-          res.end("error occurs");
-        } else {
-          const users = JSON.parse(data);
-          const userData = users.find((record) => record.id === id);
-          if (userData) {
-            res.writeHead(200, { "Content-Type": "application/json" });
-            res.end(JSON.stringify(userData));
-          } else {
-            res.writeHead(404, { "Content-Type": "text/html" });
-            res.end("User not found");
-          }
-        }
-      });
-    } else if (req.method === "POST" && req.url === "/usersApi") {
-      let data = "";
-      req.on("data", (chunk) => {
-        data += chunk;
-      });
-      req.on("end", () => {
-        const newUserData = JSON.parse(data);
-        fs.readFile(`${__dirname}/usersData.json`, "utf-8", (err, oldData) => {
-          if (err) {
-            res.writeHead(404, { "Content-Type": "text/html" });
-            res.end("error occurs");
-          } else {
-            const oldUsers = JSON.parse(oldData);
-            const newUserId = oldUsers[oldUsers.length - 1].id + 1;
-            const newUser = {
-              id: newUserId,
-              ...newUserData,
-            };
-            const addNewUser = [...oldUsers, newUser];
-            const addNewUserString = JSON.stringify(addNewUser);
-            fs.writeFile(`${__dirname}/usersData.json`, addNewUserString, (err) => {
-              if (err) {
-                res.writeHead(500, { "Content-Type": "text/html" });
-                res.end("Internal Server Error");
-              } else {
-                res.writeHead(201, { "Content-Type": "application/json" });
-                res.end(JSON.stringify(newUser));
-              }
-            });
-          }
-        });
-      });
+  app.use(cors())
+   app.get( "/usersApi",(req,res)=>{fs.readFile(`${__dirname}/usersData.json`, "utf-8", (err, data) => {
+    if (err) {
+      res.status(404).send("error occurs");
     } else {
-      res.writeHead(404, { "Content-Type": "text/html" });
-      res.end("Page not found");
-    }
-  });
-});
+      res.status(200).json(JSON.parse(data))
+      ;
+    }})
+      
+      });
+    app.get("/usersApi/:id", (req, res) => {
+      const id = parseInt(req.params.id);
+    fs.readFile(`${__dirname}/usersData.json`, "utf-8", (err, data) => {
+      if (err) {
+        res.status(404).send("error occurs");
+        
+      } else {
+        const users = JSON.parse(data);
+        const userData = users.find((record) => record.id === id);
+        if (userData) {
+          res.status(200).json(userData);
 
-server.listen(8080, "127.0.0.1", () => {
+        } else {
+          res.status(404).send("User not found");
+          
+        }
+      }
+    });}
+    )  
+    app.post ( "/usersApi", (req, res)=>{ 
+      let data = "";
+    req.on("data", (chunk) => {
+      data += chunk;
+    });
+    req.on("end", () => {
+      const newUserData = JSON.parse(data);
+      fs.readFile(`${__dirname}/usersData.json`, "utf-8", (err, oldData) => {
+        if (err) {
+          res.status(404).send("error occurs");
+         
+        } else {
+          const oldUsers = JSON.parse(oldData);
+          const newUserId = oldUsers[oldUsers.length - 1].id + 1;
+          const newUser = {
+            id: newUserId,
+            ...newUserData,
+          };
+          const addNewUser = [...oldUsers, newUser];
+          
+          fs.writeFile(`${__dirname}/usersData.json`, JSON.stringify(addNewUser), (err) => {
+            if (err) {
+              res.status(500).send("Internal Server Error");
+             
+            } else {
+              res.status(201).json(newUser);
+              
+            }
+          });
+        }
+      });
+    });}) 
+    app.put(("/usersApi/"), (req, res)=>{
+      const id =parseInt(req.url.split("/")[2])
+      let data="";
+      req.on("data", (chunk)=>{
+        data+=chunk
+      })
+      req.on("end",()=>{
+        const newUpdateData=JSON.parse(data)
+        fs.readFile(`${__dirname}/usersData.json`,"utf-8" ,(err, dataToUpdate)=>{
+          if(err){
+            res.status(404).send("error occours")
+           
+          }else{
+            const newDataToBeUpdate=JSON.parse(dataToUpdate)
+            const userIndex = newDataToBeUpdate.findIndex((record) => record.id === id);
+            if(userIndex === -1){
+              res.status(404).send("user not found ")
+              
+            }else{
+              newDataToBeUpdate[userIndex] = newUpdateData;
+              
+              fs.writeFile(`${__dirname}/usersData.json`,JSON.stringify(newDataToBeUpdate),(err)=>{
+                if(err){
+                  res.status(200).send("error occour")
+                  
+                }else{
+                  res.status(200).json(newUpdateData)
+                 
+                }
+              })
+            }
+          }
+        })
+      } )
+    })
+      
+    app.use((req, res)=>{ res.status(404).send("Page not found");})
+     
+      
+  
+app.listen(8080, "127.0.0.1", () => {
   console.log("Server is running on port 8080");
 });
+
+
+
+// else if(req.method==="DELETE" && req.url.startsWith("/usersApi/")){
+//   let id= parseInt(req.url.split("/")[2])
+//   fs.readFile(`${__dirname}/usersData.json`, "utf-8",(err, data)=>{
+//     if(err){
+//       res.writeHead(404,{"COntent-Type":"text/html"})
+//       res.end("error founded")
+//     }else{
+//       const duser=JSON.parse(data)
+//       const deluser=duser.find(data=>data.id===id)
+//       if(deluser){
+//         const deleteuser=duser.filter(data=>data.id!==id)
+//       fs.writeFile(`${__dirname}/usersData.json`,JSON.stringify(deleteuser),(err)=>{
+//        if(err){ res.writeHead(500, { "Content-Type": "text/html" });
+//         res.end("Internal Server Error");}else{
+//           res.writeHead(200, { "Content-Type": "application/json" });
+//           res.end(JSON.stringify(deleteuser));
+//         }
+//       }
+//       )
+//       }
+      
+//     }
+//   })
+// }
